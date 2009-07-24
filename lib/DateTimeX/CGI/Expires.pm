9ac823327f71f745f1ee646abac057dd34888d6c
unknown <y-shimada@.(none)>
2009-07-24 10:51:15 +0900

    initial commit

---
+++ lib/DateTimeX/CGI/Expires.pm
@@ -0,0 +1,41 @@
+package DateTimeX::CGI::Expires;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DateTimeX::CGI::Expires -
+
+=head1 SYNOPSIS
+
+  use DateTimeX::CGI::Expires;
+
+=head1 DESCRIPTION
+
+DateTimeX::CGI::Expires is
+
+=head1 METHODS
+
+=over
+
+=item xxx
+
+=back
+
+=head1 AUTHOR
+
+Yuji Shimada E<lt>xaicron {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
